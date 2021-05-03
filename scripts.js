bff1d2ff68f4f38f2a23b329984b8b7cd687e768
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2021-05-03 14:09:20 +0200

    finished email credential form

--- scripts.js
+++ scripts.js
@@ -1,13 +1,43 @@
+const form = document.getElementById('emailForm');
+const addButton = document.querySelector('.add');
+const submitButton = document.querySelector('.submit');
+
 function handleClick(event) {
   const button = event.target;
-  const form = document.querySelector('form');
   if (!button.classList.contains('expanded')) {
     button.classList.add('expanded');
+
     form.classList.remove('hide');
   } else {
     button.classList.remove('expanded');
+
+    addButton.disabled = true;
+    submitButton.disabled = true;
+
+    form.classList.remove('active');
     form.classList.add('hide');
   }
 }
 
+function handleFocus() {
+  if (form.classList.contains('active')) {
+    return;
+  }
+  form.classList.add('active');
+
+  addButton.disabled = false;
+  submitButton.disabled = false;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  // TODO: API call to open popup
+}
+
 document.getElementById('expand').addEventListener('click', handleClick);
+
+document.querySelectorAll('input').forEach((input) => {
+  input.addEventListener('focus', handleFocus);
+});
+
+form.addEventListener('submit', handleSubmit);
