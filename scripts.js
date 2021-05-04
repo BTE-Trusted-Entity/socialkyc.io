5092b980f5db0573cae33aa9b3ba6c921217e53d
Luke Grunau <lukegrunau@gmail.com>
2021-05-04 15:08:10 +0200

    SK-154: Send email overlay (#3)

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 const form = document.getElementById('emailForm');
-const addButton = document.querySelector('.add');
-const submitButton = document.querySelector('.submit');
+const addButton = document.getElementById('add');
+const submitButton = document.getElementById('submit');
+const expandButton = document.getElementById('expand');
+const overlay = document.getElementById('overlay');
 
-function handleClick(event) {
-  const button = event.target;
-
-  button.classList.toggle('expanded');
-  form.hidden = !button.classList.contains('expanded');
+function handleExpand() {
+  expandButton.classList.toggle('expanded');
+  form.hidden = !expandButton.classList.contains('expanded');
 }
 
 function handleFocus() {
@@ -16,17 +16,32 @@ function handleFocus() {
   submitButton.disabled = false;
 }
 
+function sendEmail() {
+  // TODO: send email
+  overlay.hidden = false;
+}
+
 function handleSubmit(event) {
   event.preventDefault();
 
-  window.sporranExtension.showClaimPopup({
-    'Full Name': event.target.elements.name.value,
-    Email: event.target.elements.email.value,
-  });
+  window.sporranExtension.showClaimPopup(
+    {
+      'Full Name': event.target.elements.name.value,
+      Email: event.target.elements.email.value,
+    },
+    sendEmail
+  );
 }
 
-document.getElementById('expand').addEventListener('click', handleClick);
+function handleClose() {
+  handleExpand();
+  overlay.hidden = true;
+}
+
+expandButton.addEventListener('click', handleExpand);
 
 form.addEventListener('focusin', handleFocus);
 
 form.addEventListener('submit', handleSubmit);
+
+document.getElementById('close').addEventListener('click', handleClose);
