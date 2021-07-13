f7a4ecf3020a37a05c07c26d7dc2c64311c3fa7d
Tom Adler <me@arty.name>
2021-07-13 16:47:51 +0200

    SK-356: send real request of credentials object

--- src/verifier.ts
+++ src/verifier.ts
@@ -1,6 +1,10 @@
+import { IRequestClaimsForCTypes, MessageBodyType } from '@kiltprotocol/types';
+import { Identity } from '@kiltprotocol/core';
+import Message from '@kiltprotocol/messaging';
+
 import { getSession } from './utilities/session';
-import { MessageBodyType } from '@kiltprotocol/types';
 import { initKilt } from './utilities/initKilt';
+import { email } from './CTypes/email';
 
 const form = document.getElementById('subscription-form') as HTMLFormElement;
 const success = document.getElementById('subscribed') as HTMLDivElement;
@@ -22,6 +26,20 @@ async function handleClick() {
     }
     handleSuccess();
   });
+
+  const demoIdentity = Identity.buildFromURI('//Alice');
+
+  const messageBody: IRequestClaimsForCTypes = {
+    content: [{ cTypeHash: email.hash }],
+    type: MessageBodyType.REQUEST_CLAIMS_FOR_CTYPES,
+  };
+  const message = new Message(
+    messageBody,
+    demoIdentity.getPublicIdentity(),
+    session.account,
+  );
+
+  await session.send(message);
 }
 
 signUp.addEventListener('click', handleClick);
