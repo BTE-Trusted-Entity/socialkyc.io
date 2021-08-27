1132481b229926f14eeb39e4bb6184070cc97f57
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2021-08-27 16:53:43 +0200

    Started converting backend to Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -5,42 +5,74 @@ import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 
-export default {
-  entry: {
-    requestAttestation: path.resolve('./src/requestAttestation.ts'),
-    confirmAttestation: path.resolve('./src/confirmAttestation.ts'),
-    verifier: path.resolve('./src/verifier.ts'),
-  },
-  output: {
-    filename: 'js/[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-    fallback: {
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
+export default [
+  {
+    entry: {
+      requestAttestation: path.resolve('./src/requestAttestation.ts'),
+      confirmAttestation: path.resolve('./src/confirmAttestation.ts'),
+      verifier: path.resolve('./src/verifier.ts'),
     },
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      process: ['process'],
-    }),
-    new CopyPlugin({
-      patterns: [
+    output: {
+      filename: 'js/[name].js',
+      path: path.resolve('./dist/frontend'),
+    },
+    module: {
+      rules: [
         {
-          from: path.resolve('./src/static/'),
-          to: path.resolve('./dist/'),
+          test: /\.ts$/,
+          use: 'ts-loader',
         },
       ],
-    }),
-  ],
-};
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: ['process'],
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve('./src/static/'),
+            to: path.resolve('./dist/frontend/'),
+          },
+        ],
+      }),
+    ],
+  },
+  {
+    entry: {
+      server: path.resolve('./src/server.ts'),
+    },
+    output: {
+      path: path.resolve('./dist/backend'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+      },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: ['process'],
+      }),
+    ],
+  },
+];
