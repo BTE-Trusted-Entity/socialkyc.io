ed93c54896dd7eb7e3b744896d6127e3c0f1a1a3
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2021-09-20 11:15:04 +0200

    Attempt to sign JWT with did, without library

---
+++ src/backend/endpoints/wellKnownDidConfig.ts
@@ -0,0 +1,87 @@
+import {
+  Request,
+  ResponseObject,
+  ResponseToolkit,
+  ServerRoute,
+} from '@hapi/hapi';
+import { KeyRelationship } from '@kiltprotocol/types';
+import { configuration } from '../utilities/configuration';
+import { fullDidPromise } from '../utilities/fullDid';
+import { authenticationKeystore } from '../utilities/keystores';
+
+const signingAlg = 'rs256';
+
+function base64ToUInt8Array(base64: string) {
+  return Uint8Array.from(Buffer.from(base64, 'base64'));
+}
+
+async function didConfigResource() {
+  const fullDidDetails = await fullDidPromise;
+  const { authentication } = KeyRelationship;
+
+  const publicKey = fullDidDetails.getKeyIds(authentication)[0];
+
+  const header = {
+    alg: signingAlg,
+    kid: publicKey,
+  };
+
+  const payload = {
+    iss: configuration.did,
+    sub: configuration.did,
+    vc: {
+      '@context': [
+        'https://www.w3.org/2018/credentials/v1',
+        'https://identity.foundation/.well-known/did-configuration/v1',
+      ],
+      credentialSubject: {
+        id: configuration.did,
+        origin: configuration.baseUri,
+      },
+      expirationDate: new Date(
+        Date.now() + 1000 * 60 * 60 * 24 * 365 * 5,
+      ).toString(), // 5 years
+      issuanceDate: new Date().toString(),
+      issuer: configuration.did,
+      type: ['VerifiableCredential', 'DomainLinkageCredential'],
+    },
+  };
+
+  const base64UrlHeader = Buffer.from(JSON.stringify(header)).toString(
+    'base64url',
+  );
+
+  const base64UrlPayload = Buffer.from(JSON.stringify(payload)).toString(
+    'base64url',
+  );
+
+  const unsignedJWT = `${base64UrlHeader}.${base64UrlPayload}`;
+
+  const response = await authenticationKeystore.sign({
+    alg: signingAlg,
+    data: base64ToUInt8Array(unsignedJWT),
+    publicKey: base64ToUInt8Array(publicKey),
+  });
+
+  const base64UrlSignature = Buffer.from(response.data).toString('base64url');
+
+  const signedJWT = `${base64UrlHeader}.${base64UrlPayload}.${base64UrlSignature}`;
+
+  return {
+    '@context': 'https://identity.foundation/.well-known/did-configuration/v1',
+    linked_dids: [signedJWT],
+  };
+}
+
+async function handler(
+  request: Request,
+  h: ResponseToolkit,
+): Promise<ResponseObject> {
+  return h.response(await didConfigResource());
+}
+
+export const wellKnownDidConfig: ServerRoute = {
+  method: 'GET',
+  path: '/.well-known/did-configuration.json',
+  handler,
+};
