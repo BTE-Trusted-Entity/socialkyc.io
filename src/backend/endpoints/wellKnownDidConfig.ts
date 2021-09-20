560afbb2b058dcad29b53b13b6675748f92f9d2f
Luke Grunau <lukegrunau@Lukes-MacBook-Pro.local>
2021-09-20 23:54:30 +0200

    Attested claim to verifiable credential

--- src/backend/endpoints/wellKnownDidConfig.ts
+++ src/backend/endpoints/wellKnownDidConfig.ts
@@ -4,77 +4,55 @@ import {
   ResponseToolkit,
   ServerRoute,
 } from '@hapi/hapi';
-import { KeyRelationship } from '@kiltprotocol/types';
+import {
+  Claim,
+  RequestForAttestation,
+  Attestation,
+  AttestedClaim,
+} from '@kiltprotocol/core';
+import VCUtils from '@kiltprotocol/vc-export';
 import { configuration } from '../utilities/configuration';
-import { fullDidPromise } from '../utilities/fullDid';
-import { authenticationKeystore } from '../utilities/keystores';
+import { domainLinkage } from '../CTypes/domainLinkage';
 
-const signingAlg = 'rs256';
+async function attestDomainLinkage() {
+  const claimContents = {
+    id: configuration.did,
+    origin: configuration.baseUri,
+  };
 
-function base64ToUInt8Array(base64: string) {
-  return Uint8Array.from(Buffer.from(base64, 'base64'));
-}
+  const claim = Claim.fromCTypeAndClaimContents(
+    domainLinkage,
+    claimContents,
+    configuration.did,
+  );
 
-function hexToUInt8Array(hex: string) {
-  return Uint8Array.from(Buffer.from(hex, 'hex'));
+  const requestForAttestation = RequestForAttestation.fromClaim(claim);
+
+  if (!RequestForAttestation.isIRequestForAttestation(requestForAttestation)) {
+    throw new Error('Invalid request for attestation');
+  }
+
+  const attestation = Attestation.fromRequestAndDid(
+    requestForAttestation,
+    configuration.did,
+  );
+
+  return AttestedClaim.fromRequestAndAttestation(
+    requestForAttestation,
+    attestation,
+  );
 }
 
 async function didConfigResource() {
-  const fullDidDetails = await fullDidPromise;
-  const { authentication } = KeyRelationship;
+  const attestedClaim = await attestDomainLinkage();
+  const VC = VCUtils.fromAttestedClaim(attestedClaim);
+  console.log('VC:', VC);
+  return VC;
 
-  const publicKey = fullDidDetails.getKeys(authentication)[0];
-
-  const header = {
-    alg: signingAlg,
-    kid: publicKey.id,
-  };
-
-  const payload = {
-    iss: configuration.did,
-    sub: configuration.did,
-    vc: {
-      '@context': [
-        'https://www.w3.org/2018/credentials/v1',
-        'https://identity.foundation/.well-known/did-configuration/v1',
-      ],
-      credentialSubject: {
-        id: configuration.did,
-        origin: configuration.baseUri,
-      },
-      expirationDate: new Date(
-        Date.now() + 1000 * 60 * 60 * 24 * 365 * 5,
-      ).toString(), // 5 years
-      issuanceDate: new Date().toString(),
-      issuer: configuration.did,
-      type: ['VerifiableCredential', 'DomainLinkageCredential'],
-    },
-  };
-
-  const base64UrlHeader = Buffer.from(JSON.stringify(header)).toString(
-    'base64url',
-  );
-
-  const base64UrlPayload = Buffer.from(JSON.stringify(payload)).toString(
-    'base64url',
-  );
-
-  const unsignedJWT = `${base64UrlHeader}.${base64UrlPayload}`;
-
-  const response = await authenticationKeystore.sign({
-    alg: signingAlg,
-    data: base64ToUInt8Array(unsignedJWT),
-    publicKey: hexToUInt8Array(publicKey.publicKeyHex),
-  });
-
-  const base64UrlSignature = Buffer.from(response.data).toString('base64url');
-
-  const signedJWT = `${base64UrlHeader}.${base64UrlPayload}.${base64UrlSignature}`;
-
-  return {
-    '@context': 'https://identity.foundation/.well-known/did-configuration/v1',
-    linked_dids: [signedJWT],
-  };
+  // return {
+  //   '@context': 'https://identity.foundation/.well-known/did-configuration/v1',
+  //   linked_dids: [signedCredential],
+  // };
 }
 
 async function handler(
