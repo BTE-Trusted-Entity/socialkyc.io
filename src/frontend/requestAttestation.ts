f7708f0c1582d4aaf4883c7aa076dd97c9facd5a
Luke Grunau <lukegrunau@gmail.com>
2021-10-13 11:53:08 +0200

    SK-398: Handle dApp closure while popup open (#122)

--- src/frontend/requestAttestation.ts
+++ src/frontend/requestAttestation.ts
@@ -30,49 +30,60 @@ function handleFocus() {
   submitButton.disabled = false;
 }
 
+function handleBeforeUnload(event: Event) {
+  event.preventDefault();
+  event.returnValue = false;
+}
+
 async function handleSubmit(event: Event) {
   event.preventDefault();
+  window.addEventListener('beforeunload', handleBeforeUnload);
+  try {
+    const session = await getSession();
+    await session.listen(async (message) => {
+      const result = await ky.post('/request-attestation', { json: message });
 
-  const session = await getSession();
-  await session.listen(async (message) => {
-    const result = await ky.post('/request-attestation', { json: message });
+      window.removeEventListener('beforeunload', handleBeforeUnload);
 
-    if (result.status === StatusCodes.ACCEPTED) {
-      console.log('Terms rejected');
-    }
+      if (result.status === StatusCodes.ACCEPTED) {
+        console.log('Terms rejected');
+      }
 
-    if (result.status !== StatusCodes.OK) {
-      console.log('Not attested');
-      return;
-    }
+      if (result.status !== StatusCodes.OK) {
+        console.log('Not attested');
+        return;
+      }
 
-    const email = await result.text();
-    document
-      .getElementById('sent')
-      ?.insertAdjacentHTML(
-        'afterend',
-        `<p>We've sent an email to <strong>${email}</strong></p>`,
-      );
+      const email = await result.text();
+      document
+        .getElementById('sent')
+        ?.insertAdjacentHTML(
+          'afterend',
+          `<p>We've sent an email to <strong>${email}</strong></p>`,
+        );
 
-    if (overlay) {
-      overlay.hidden = false;
-    }
-  });
+      if (overlay) {
+        overlay.hidden = false;
+      }
+    });
 
-  const target = event.target as unknown as {
-    elements: Record<string, HTMLInputElement>;
-  };
-  const json = {
-    name: target.elements?.name?.value,
-    email: target.elements?.email?.value,
-    did: session.identity,
-  };
+    const target = event.target as unknown as {
+      elements: Record<string, HTMLInputElement>;
+    };
+    const json = {
+      name: target.elements?.name?.value,
+      email: target.elements?.email?.value,
+      did: session.identity,
+    };
 
-  const message = (await ky
-    .post('/quote', { json })
-    .json()) as IEncryptedMessage;
+    const message = (await ky
+      .post('/quote', { json })
+      .json()) as IEncryptedMessage;
 
-  await session.send(message);
+    await session.send(message);
+  } catch {
+    window.removeEventListener('beforeunload', handleBeforeUnload);
+  }
 }
 
 function handleClose() {
