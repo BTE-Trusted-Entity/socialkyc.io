12bd12f013a583ca3d5aa6b3fbe1f698c4c44bb4
Tom Adler <me@arty.name>
2021-10-20 14:12:12 +0200

    build backend using tsc

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,38 @@
 import path from 'path';
 import CopyPlugin from 'copy-webpack-plugin';
-import nodeExternals from 'webpack-node-externals';
 
-export default [
-  {
-    entry: {
-      App: path.resolve('./src/frontend/App.tsx'),
-      confirmAttestation: path.resolve('./src/frontend/confirmAttestation.ts'),
-      verifier: path.resolve('./src/frontend/verifier.ts'),
-    },
-    output: {
-      filename: 'js/[name].js',
-      path: path.resolve('./dist/frontend'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js'],
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.resolve('./src/frontend/static/'),
-            to: path.resolve('./dist/frontend/'),
-          },
-        ],
-      }),
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+export default {
+  mode: isDevelopment ? 'development' : 'production',
+  entry: {
+    App: path.resolve('./src/frontend/App.tsx'),
+    confirmAttestation: path.resolve('./src/frontend/confirmAttestation.ts'),
+    verifier: path.resolve('./src/frontend/verifier.ts'),
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve('./dist/frontend'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
     ],
   },
-  {
-    externalsType: 'commonjs',
-    externalsPresets: { node: true },
-    externals: [nodeExternals()],
-    entry: {
-      server: path.resolve('./src/backend/server.ts'),
-    },
-    output: {
-      path: path.resolve('./dist/backend'),
-      filename: '[name].cjs',
-      chunkFormat: 'module',
-    },
-    module: {
-      rules: [
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
         {
-          test: /\.ts$/,
-          use: 'ts-loader',
+          from: path.resolve('./src/frontend/static/'),
+          to: path.resolve('./dist/frontend/'),
         },
       ],
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-  },
-];
+    }),
+  ],
+};
