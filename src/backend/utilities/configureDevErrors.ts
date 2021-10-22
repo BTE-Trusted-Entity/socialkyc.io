3ec508575b81d85f97ec6236a4c315c8a731ec7c
Tom Adler <me@arty.name>
2021-10-22 10:44:27 +0200

    build backend with parcel as well

--- src/backend/utilities/configureDevErrors.ts
+++ src/backend/utilities/configureDevErrors.ts
@@ -1,13 +1,11 @@
 import { Server } from '@hapi/hapi';
+import devErrors from 'hapi-dev-errors';
 
 import { configuration } from './configuration';
 
 export async function configureDevErrors(server: Server): Promise<void> {
-  try {
-    const devErrors = (await import('hapi-dev-errors')).default;
+  if (process.env.NODE_ENV !== 'production') {
     const options = { showErrors: !configuration.isProduction };
     await server.register({ plugin: devErrors, options });
-  } catch {
-    // ignore the package missing in production mode
   }
 }
