2d307b8116a79e264477b377a080dca12551f52e
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2021-11-03 15:32:28 +0100

    Refresh Tx index

--- src/backend/endpoints/attestationEmail.ts
+++ src/backend/endpoints/attestationEmail.ts
@@ -38,6 +38,10 @@ async function attestClaim(
 
   const { fullDid } = await fullDidPromise;
   const { identity } = await keypairsPromise;
+
+  // TODO: Remove when we get SDK upgrade which includes this call in authorizeExtrinsic
+  await fullDid.refreshTxIndex();
+
   const extrinsic = await fullDid.authorizeExtrinsic(
     tx,
     assertionKeystore,
