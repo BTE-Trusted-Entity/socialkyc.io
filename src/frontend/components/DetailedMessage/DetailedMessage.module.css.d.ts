e95509e1a781891d6bbbba0838eac2445aec132a
Tom Adler <me@arty.name>
2021-12-10 16:49:52 +0100

    moved checkmark into DetailedMessage

--- src/frontend/components/DetailedMessage/DetailedMessage.module.css.d.ts
+++ src/frontend/components/DetailedMessage/DetailedMessage.module.css.d.ts
@@ -2,6 +2,7 @@ export const __esModule: true;
 export const container: string;
 export const heading: string;
 export const message: string;
+export const icon: string;
 export const spinner: string;
 export const checkmark: string;
 export const exclamation: string;
