0ca3909e84748722ae25e18bdecd637e47795c61
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2022-02-08 15:18:52 +0100

    no revoke endpoint

--- src/backend/github/githubEndpoints.ts
+++ src/backend/github/githubEndpoints.ts
@@ -7,7 +7,6 @@ export const githubEndpoints = {
   authorize: 'https://github.com/login/oauth/authorize',
   token: 'https://github.com/login/oauth/access_token',
   profile: 'https://api.github.com/user',
-  revoke: `https://github.com/applications/${configuration.github.clientId}/grant`,
 
   redirectUri: `${configuration.baseUri}${paths.oauth.github}`,
 };
