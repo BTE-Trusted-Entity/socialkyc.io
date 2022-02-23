6a578a253c56ce26f67962da419531afb1bc40bd
Tom Adler <me@arty.name>
2022-02-23 12:26:58 +0100

    SK-726: clean up key update code

--- src/backend/utilities/keypairs.ts
+++ src/backend/utilities/keypairs.ts
@@ -1,6 +1,5 @@
 import { Keyring } from '@polkadot/keyring';
-import { IIdentity, KeyRelationship, KeyringPair } from '@kiltprotocol/types';
-import { DefaultResolver } from '@kiltprotocol/did';
+import { IIdentity, KeyringPair } from '@kiltprotocol/types';
 import { Balance, BalanceUtils } from '@kiltprotocol/core';
 import {
   blake2AsU8a,
@@ -27,44 +26,6 @@ function makeKeyring(): Keyring {
 
 export function getKeypairByBackupPhrase(backupPhrase: string): KeyringPair {
   return makeKeyring().addFromUri(backupPhrase);
-}
-
-function getLegacyKeyAgreementKey(identity: KeyringPair) {
-  const keyAgreement = naclBoxPairFromSecret(
-    identity
-      .derive('//did//keyAgreement//0')
-      .encryptMessage(
-        new Uint8Array(24).fill(0),
-        new Uint8Array(24).fill(0),
-        new Uint8Array(24).fill(0),
-      )
-      .slice(24), // first 24 bytes are the nonce
-  );
-  return {
-    ...keyAgreement,
-    type: 'x25519',
-  };
-}
-
-async function useLegacy() {
-  const { did } = configuration;
-  const didDocument = await DefaultResolver.resolveDoc(did);
-  if (!didDocument || !didDocument.details) {
-    throw new Error(`Could not resolve ${did} after key upgrade`);
-  }
-
-  const existingKey = didDocument.details
-    .getKeys(KeyRelationship.keyAgreement)
-    .pop();
-  if (!existingKey) {
-    throw new Error('Key agreement key not found');
-  }
-
-  const legacyKey =
-    '0xf2c90875e0630bd1700412341e5e9339a57d2fefdbba08de1cac8db5b4145f6e';
-  const storedLegacyKey = existingKey.publicKeyHex === legacyKey;
-
-  return storedLegacyKey && !configuration.storeDidAndCTypes;
 }
 
 async function reportBalance(address: IIdentity['address']) {
@@ -99,9 +60,7 @@ export const keypairsPromise = (async () => {
     identity,
     authentication,
     assertion,
-    keyAgreement: (await useLegacy())
-      ? getLegacyKeyAgreementKey(identity)
-      : keyAgreement,
+    keyAgreement,
   };
 })();
 
