58bff6919a9fc47f94aef1532e74530a42a913bc
Luke Grunau <lukegrunau@gmail.com>
2022-03-01 11:16:11 +0100

    SK-625: Low balance alert (#621)

--- src/backend/utilities/keypairs.ts
+++ src/backend/utilities/keypairs.ts
@@ -1,6 +1,5 @@
 import { Keyring } from '@polkadot/keyring';
-import { IIdentity, KeyringPair } from '@kiltprotocol/types';
-import { Balance, BalanceUtils } from '@kiltprotocol/core';
+import { KeyringPair } from '@kiltprotocol/types';
 import {
   blake2AsU8a,
   ed25519PairFromSeed,
@@ -13,7 +12,6 @@ import {
 import { initKilt } from './initKilt';
 import { configuration } from './configuration';
 import { exitOnError } from './exitOnError';
-import { logger } from './logger';
 
 const ss58Format = 38;
 
@@ -26,15 +24,6 @@ function makeKeyring(): Keyring {
 
 export function getKeypairByBackupPhrase(backupPhrase: string): KeyringPair {
   return makeKeyring().addFromUri(backupPhrase);
-}
-
-async function reportBalance(address: IIdentity['address']) {
-  const balances = await Balance.getBalances(address);
-
-  const free = BalanceUtils.formatKiltBalance(balances.free);
-  const reserved = BalanceUtils.formatKiltBalance(balances.reserved);
-
-  logger.info(`Free: ${free}, bonded: ${reserved}`);
 }
 
 export const keypairsPromise = (async () => {
@@ -53,8 +42,6 @@ export const keypairsPromise = (async () => {
     ...naclBoxPairFromSecret(blake2AsU8a(secretKey)),
     type: 'x25519',
   };
-
-  await reportBalance(identity.address);
 
   return {
     identity,
---
+++ src/backend/utilities/reportBalance.ts
@@ -0,0 +1,69 @@
+import { Balance, BalanceUtils } from '@kiltprotocol/core';
+
+import { SendEmailCommand } from '@aws-sdk/client-ses';
+
+import { sesClient } from '../email/sesClient';
+
+import { logger } from './logger';
+import { configuration } from './configuration';
+import { keypairsPromise } from './keypairs';
+
+const REPORT_THRESHOLD = BalanceUtils.toFemtoKilt(1000);
+const REPORT_FREQUENCY = 24 * 60 * 60 * 1000;
+
+async function sendLowBalanceAlert(balance: string) {
+  const Data = `The SocialKYC balance is currently low. Please add more KILT coins.
+  
+Current balance: ${balance}
+  
+This is an automatically generated email.`;
+
+  const { lowBalanceAlertRecipients } = configuration;
+
+  const command = new SendEmailCommand({
+    Destination: {
+      ToAddresses: lowBalanceAlertRecipients.split(','),
+    },
+    Source: '"SocialKYC" <attester@socialkyc.io>',
+    Message: {
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'SocialKYC – Low Balance Alert',
+      },
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data,
+        },
+      },
+    },
+  });
+
+  await sesClient.send(command);
+}
+
+function sleep(milliseconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+export async function reportBalance(): Promise<void> {
+  while (true) {
+    try {
+      const { identity } = await keypairsPromise;
+      const balances = await Balance.getBalances(identity.address);
+
+      const free = BalanceUtils.formatKiltBalance(balances.free);
+      const reserved = BalanceUtils.formatKiltBalance(balances.reserved);
+
+      logger.info(`Free: ${free}, bonded: ${reserved}`);
+
+      if (balances.free.lt(REPORT_THRESHOLD)) {
+        await sendLowBalanceAlert(free);
+      }
+    } catch (error) {
+      logger.error(error, 'Error getting attester balance');
+    }
+
+    await sleep(REPORT_FREQUENCY);
+  }
+}
