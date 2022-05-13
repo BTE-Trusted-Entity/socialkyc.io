3227d17708d6016a5aa385d42d3142a788e8440b
usmangeek <usman@galaniprojects.de>
2022-05-13 09:52:34 +0200

    fix: remove revoke url

--- src/backend/instagram/instagramEndpoints.ts
+++ src/backend/instagram/instagramEndpoints.ts
@@ -5,7 +5,6 @@ export const instagramEndpoints = {
   authorize: 'https://api.instagram.com/oauth/authorize',
   token: 'https://api.instagram.com/oauth/access_token',
   profile: 'https://graph.instagram.com',
-  revoke: 'https://api.instagram.com/oauth/revoke',
 
   redirectUri: `${configuration.baseUri}${paths.oauth.instagram}`,
 };
