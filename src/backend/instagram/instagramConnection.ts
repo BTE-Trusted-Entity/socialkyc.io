350c5b5725a8c1fe7d5ab54beeb27a1a6c5c5a71
usmangeek <usman@galaniprojects.de>
2022-05-18 15:18:10 +0200

    update instagram connection test

---
+++ src/backend/instagram/instagramConnection.ts
@@ -0,0 +1,49 @@
+import got, { HTTPError } from 'got';
+
+import { trackConnectionState } from '../utilities/trackConnectionState';
+import { logger } from '../utilities/logger';
+import { configuration } from '../utilities/configuration';
+
+import { instagramEndpoints } from './instagramEndpoints';
+
+export const instagramConnectionState = trackConnectionState(10 * 60 * 1000);
+
+const isInvalidCode = (body: string) => {
+  return JSON.parse(body).error_message === 'Invalid authorization code';
+};
+
+export async function canAccessInstagram() {
+  try {
+    await got
+      .post(instagramEndpoints.token, {
+        form: {
+          client_id: configuration.instagram.clientId,
+          client_secret: configuration.instagram.secret,
+          code: 'code',
+          grant_type: 'authorization_code',
+          redirect_uri: instagramEndpoints.redirectUri,
+        },
+      })
+      .json();
+    instagramConnectionState.on();
+  } catch (error) {
+    if (
+      error instanceof HTTPError &&
+      isInvalidCode(error.response.body as string)
+    ) {
+      instagramConnectionState.on();
+      return;
+    }
+    instagramConnectionState.off();
+    logger.error(error, 'Error connecting to Instagram');
+    throw error;
+  }
+}
+
+export function checkInstagramConnection() {
+  setInterval(async () => {
+    try {
+      await canAccessInstagram();
+    } catch {}
+  }, 5 * 60 * 1000);
+}
