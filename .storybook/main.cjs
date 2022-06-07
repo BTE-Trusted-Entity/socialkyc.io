784ccb1efd79c643e56f4a657650a8003d2ed787
Tom Adler <me@arty.name>
2022-06-07 15:08:05 +0200

    remove storybook workaround (#906)

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -28,46 +28,6 @@ module.exports = {
     const cssLoaderOptions = cssRule.use[1].options;
     cssLoaderOptions.modules = { namedExport: true };
 
-    workaroundRequireNotDefined(config);
-
     return config;
   },
 };
-
-/** Workaround for https://github.com/storybookjs/storybook/issues/14877 */
-function workaroundRequireNotDefined(config) {
-  function replaceFileExtension(filePath, newExtension) {
-    const { name, root, dir } = path.parse(filePath);
-    return path.format({
-      name,
-      root,
-      dir,
-      ext: newExtension,
-    });
-  }
-
-  // Find the plugin instance that needs to be mutated
-  const virtualModulesPlugin = config.plugins.find(
-    (plugin) => plugin.constructor.name === "VirtualModulesPlugin"
-  );
-
-  // Change the file extension to .cjs for all files that end with "generated-stories-entry.js"
-  virtualModulesPlugin._staticModules = Object.fromEntries(
-    Object.entries(virtualModulesPlugin._staticModules).map(
-      ([key, value]) => {
-        if (key.endsWith("generated-stories-entry.js")) {
-          return [replaceFileExtension(key, ".cjs"), value];
-        }
-        return [key, value];
-      }
-    )
-  );
-
-  // Change the entry points to point to the appropriate .cjs files
-  config.entry = config.entry.map((entry) => {
-    if (entry.endsWith("generated-stories-entry.js")) {
-      return replaceFileExtension(entry, ".cjs");
-    }
-    return entry;
-  });
-}
