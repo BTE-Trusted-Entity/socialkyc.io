e9ae65caf3016979bef114469e75760322f09bd1
Raphael Flechtner <raphael@kilt.io>
2022-09-19 10:50:00 +0200

    chore: simplify kiltInit

--- src/backend/utilities/initKilt.ts
+++ src/backend/utilities/initKilt.ts
@@ -1,11 +1,14 @@
-import { init, connect as kiltConnect } from '@kiltprotocol/core';
+import { connect } from '@kiltprotocol/core';
 
 import { configuration } from './configuration';
 import { logger } from './logger';
 import { trackConnectionState } from './trackConnectionState';
 
 export async function initKilt(): Promise<void> {
-  await init();
+  const api = await connect(configuration.blockchainEndpoint);
+  api.on('disconnected', disconnectHandler);
+  api.on('connected', () => blockchainConnectionState.on());
+  api.on('error', (error) => logger.error(error));
 }
 
 export const blockchainConnectionState = trackConnectionState(60 * 1000);
@@ -13,11 +16,4 @@ export const blockchainConnectionState = trackConnectionState(60 * 1000);
 export async function disconnectHandler(value?: string) {
   blockchainConnectionState.off();
   logger.warn(value, 'Received disconnect event from the blockchain');
-}
-
-export async function connect() {
-  const api = await kiltConnect(configuration.blockchainEndpoint);
-  api.on('disconnected', disconnectHandler);
-  api.on('connected', () => blockchainConnectionState.on());
-  api.on('error', (error) => logger.error(error));
 }
