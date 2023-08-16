09534a55074f6b748a8c0117ac9709731eea0756
Tom Adler <me@arty.name>
2023-08-16 10:29:35 +0200

    storybook cleanup (#1542)

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,16 +9,6 @@ export default {
     '@storybook/addon-viewport',
     '@storybook/addon-controls',
     '@storybook/addon-toolbars',
+    { name: '@storybook/addon-styling', options: { cssModules: { namedExport: true } } },
   ],
-  async webpackFinal(config) {
-    const cssRule = config.module.rules.find(
-      ({ test }) => test.toString() === '/\\.css$/',
-    );
-
-    const cssLoaderOptions = cssRule.use[1].options;
-    cssLoaderOptions.modules = {
-      namedExport: true,
-    };
-    return config;
-  },
 };
