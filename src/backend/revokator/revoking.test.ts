ee18aa9d936c04fb594d40cdb9ead6028081487f
kilted-andres <andres@trusted-entity.io>
2023-08-22 18:28:59 +0200

    feat: prepare Transactions

--- src/backend/revokator/revoking.test.ts
+++ src/backend/revokator/revoking.test.ts
@@ -5,11 +5,20 @@
 import { describe, it } from '@jest/globals';
 
 import { scanForOldCredentials } from './scanForOldCredentials';
+import { prepareRevocations } from './prepareTransactions';
 
 describe('scan for old credential attestations', () => {
-  it('for the first 100.000, there should be 6 attestations on peregrine', async () => {
+  it('for the first 100.000 Bl, there should be 6 attestations on peregrine', async () => {
     const extrinsicsHashesArray = await scanForOldCredentials(0, 100000, true);
 
     expect(extrinsicsHashesArray.length).toBe(6);
+  });
+});
+
+describe('prepare transactions to revoke', () => {
+  it('for the first 100.000 Bl, there should be 6 attestations on peregrine', async () => {
+    const submittableExtrinsics = await prepareRevocations(0, 100000);
+
+    expect(submittableExtrinsics.length).toBe(6);
   });
 });
