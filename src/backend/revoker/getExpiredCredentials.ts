ebee33976f09780923b98940d0aa43aabbf5fffd
kilted-andres <andres@trusted-entity.io>
2023-09-04 18:43:42 +0200

    feat: function deduceWishedState

--- src/backend/revoker/getExpiredCredentials.ts
+++ src/backend/revoker/getExpiredCredentials.ts
@@ -41,12 +41,9 @@ export async function* expiredCredentialsGetter(
         `Could not assign a validity state for this credential ${attestationInfo.claimHash}`,
       );
     }
-    const dateOfIssuance = attestationInfo.createdAt.getTime();
-    const dateNow = Date.now();
-    const millisecondsInAYear = new Date('1971').getTime();
 
     // if younger than a year
-    if (dateOfIssuance > dateNow - 1 * millisecondsInAYear) {
+    if (deduceWishedState(attestationInfo) === 'valid') {
       logger.debug(
         'No more credentials younger than a year attested by SocialKYC.',
       );
@@ -88,11 +85,6 @@ async function assignState(
 
   attestationInfo.state = await readCurrentState(attestationInfo);
 
-  if (!attestationInfo.state) {
-    throw new Error(
-      `Could not assign any state to the attestation: ${attestationInfo.claimHash}`,
-    );
-  }
   return attestationInfo;
 }
 
@@ -127,7 +119,36 @@ export async function readCurrentState(
   }
 
   // Else:
+  // including attestationInfo.state === undefined
   throw new Error(
     `Could not assign any state to the attestation: ${attestationInfo.claimHash}`,
   );
+}
+
+/**
+ * Decides which state an Attestation should have based on time.
+ *
+ * If it is younger than 1 year, it can still be valid.
+ * If it is older than 1 year, it should be revoked.
+ * If it is older than 2 years, it should be removed.
+ *
+ * @param attestationInfo
+ * @returns one of the validity states: 'valid' | 'revoked' | 'removed'
+ */
+export function deduceWishedState(
+  attestationInfo: AttestationInfo,
+): AttestationInfo['state'] {
+  const dateNow = Date.now();
+  const millisecondsInAYear = new Date('1971').getTime();
+
+  const removalCutoffDate = new Date(dateNow - 2 * millisecondsInAYear);
+  const revocationCutoffDate = new Date(dateNow - 1 * millisecondsInAYear);
+
+  if (attestationInfo.createdAt < removalCutoffDate) {
+    return 'removed';
+  }
+  if (attestationInfo.createdAt < revocationCutoffDate) {
+    return 'revoked';
+  }
+  return 'valid';
 }
