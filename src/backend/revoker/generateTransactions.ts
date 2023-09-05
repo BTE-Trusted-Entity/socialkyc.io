e3688e91630404456fc018ba63948108c6ea98b2
kilted-andres <andres@trusted-entity.io>
2023-09-05 13:56:57 +0200

    feat: flatMap instead of for...of

--- src/backend/revoker/generateTransactions.ts
+++ src/backend/revoker/generateTransactions.ts
@@ -14,33 +14,23 @@ import { deduceWishedState } from './stateIdentifiers';
 export async function generateTransactions(
   arrayOfAttestationsInfo: AttestationInfo[],
 ): Promise<SubmittableExtrinsic[]> {
-  const arrayOfTransactions: SubmittableExtrinsic[] = [];
   await initKilt();
   const api = ConfigService.get('api');
 
-  for (const attestationInfo of arrayOfAttestationsInfo) {
+  return arrayOfAttestationsInfo.flatMap((attestationInfo) => {
     const wishedState = deduceWishedState(attestationInfo);
 
     if (wishedState === 'removed') {
       // If the attestation is to be removed, create a `remove` tx,
       // which revokes and removes the attestation in one go.
-      const transaction = api.tx.attestation.remove(
-        attestationInfo.claimHash,
-        null,
-      );
-      arrayOfTransactions.push(transaction);
+      return [api.tx.attestation.remove(attestationInfo.claimHash, null)];
     }
 
     if (wishedState === 'revoked') {
       // Otherwise, simply revoke the attestation but leave it on chain.
       // Hence, the storage is not cleared and the deposit not returned.
-      const transaction = api.tx.attestation.revoke(
-        attestationInfo.claimHash,
-        null,
-      );
-      arrayOfTransactions.push(transaction);
+      return [api.tx.attestation.revoke(attestationInfo.claimHash, null)];
     }
-  }
-
-  return arrayOfTransactions;
+    return [];
+  });
 }
