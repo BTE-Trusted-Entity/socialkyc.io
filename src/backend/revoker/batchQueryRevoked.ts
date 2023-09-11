473ae31611a4cb20bfcb98455f3a5815232bff78
kilted-andres <andres@trusted-entity.io>
2023-09-11 12:10:39 +0200

    feat: name the returned string

--- src/backend/revoker/batchQueryRevoked.ts
+++ src/backend/revoker/batchQueryRevoked.ts
@@ -9,9 +9,11 @@ import { initKilt } from '../utilities/initKilt';
 /**
  * Queries the revoked status of an array of attestations from the blockchain in bulk
  * @param claimHashes
- * @returns Map of claimHashes to revoked status (null means absent from the blockchain)
+ * @returns Map of claimHashes to revoked status (null means removed from the blockchain)
  */
-export async function batchQueryRevoked(claimHashes: HexString[]) {
+export async function batchQueryRevoked(
+  claimHashes: HexString[],
+): Promise<{ [claimHash: HexString]: boolean | null }> {
   await initKilt();
   const api = ConfigService.get('api');
 
