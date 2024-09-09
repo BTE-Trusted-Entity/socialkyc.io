4f66c28c316a408ac8491b47a46114d2f399e19e
kilted-andres <andres@kilt.io>
2024-09-09 10:21:06 +0200

    fix: use right environment constant

--- src/backend/revoker/indexer/watchIndexer.ts
+++ src/backend/revoker/indexer/watchIndexer.ts
@@ -1,15 +1,15 @@
-import { configuration } from '../configuration';
-import { sleep } from '../sleep';
+import { configuration } from '../../utilities/configuration';
+import { sleep } from '../../utilities/sleep';
 
 import { queryCTypes } from './queryCTypes';
 import { updateAttestationsCreated } from './updateAttestationsCreated';
 
-const { isTest } = configuration;
+const { isTestEnvironment } = configuration;
 
 const SCAN_INTERVAL_MS = 10 * 60 * 1000;
 
 export function watchIndexer() {
-  if (isTest) {
+  if (isTestEnvironment) {
     return;
   }
   (async () => {
